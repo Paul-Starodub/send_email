044575e4f41321b4a33ff851ee6371d87de8f0cc
Paul Starodub <amorallex@gmail.com>
2023-03-28 17:19:11 +0400

    Updated settings & __init__ with configuration for celery

--- send_email/settings.py
+++ send_email/settings.py
@@ -134,3 +134,13 @@ EMAIL_HOST_PASSWORD = os.environ.get(
     "EMAIL_HOST_PASSWORD"
 )  # this password was generated from password of email
 EMAIL_PORT = 587
+
+REDIS_HOST = "0.0.0.0"
+REDIS_PORT = "6379"
+
+CELERY_BROKER_URL = "redis://" + REDIS_HOST + ":" + REDIS_PORT + "/0"
+CELERY_BROKER_TRANSPORT_OPTIONS = {"visibility_timeout": 3600}
+CELERY_RESULT_BACKEND = "redis://" + REDIS_HOST + ":" + REDIS_PORT + "/0"
+CELERY_ACCEPT_CONTENT = ["application/json"]
+CELERY_TASK_SERIALIZER = "json"
+CELERY_RESULT_SERIALIZER = "json"
