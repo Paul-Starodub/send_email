c378357042ef776dad5b2c8316d39d810ba940ff
Paul Starodub <amorallex@gmail.com>
2023-03-28 11:24:25 +0400

    Updated forms & urls & views & settings html

--- main/views.py
+++ main/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.views import generic
+from main.forms import ContactForm
+from main.models import Contact
+from servise import send
 
-# Create your views here.
+
+class ContactView(generic.CreateView):
+    """Displaying the email subscription form"""
+
+    model = Contact
+    form_class = ContactForm
+    success_url = "/"
+    template_name = "main/contact.html"
+
+    def form_valid(self, form: ContactForm) -> None:
+        form.save()
+        send(form.instance.email)
+        return super().form_valid(form)
