c975dde4f405f24819407f1b38880773c2625b27
Paul Starodub <amorallex@gmail.com>
2023-03-28 20:20:55 +0400

    Updated celery & tasks & env.sample

--- main/tasks.py
+++ main/tasks.py
@@ -1,4 +1,6 @@
+from django.core.mail import send_mail
 from send_email.celery import app
+from .models import Contact
 
 from .servise import send
 
@@ -6,3 +8,15 @@ from .servise import send
 @app.task
 def send_spam_email(user_email: str) -> None:
     send(user_email)
+
+
+@app.task
+def send_beat_email():
+    for contact in Contact.objects.all():
+        send_mail(
+            "We will send you spam",
+            "Very active spam every 2 min",
+            "allex@gmail.com",
+            [contact.email],
+            fail_silently=False,
+        )
